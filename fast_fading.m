166202bd73a26950d39f1d22719000bcaf507263
obolleurs <obolleurs@gmail.com>
2025-01-11 16:17:56 +0100

    Added plots and histograms

--- fast_fading.m
+++ fast_fading.m
@@ -1,3 +1,16 @@
 %% Load dataset
-
+clear all 
+close all
 load("group-02-02-2024-2025.mat")
+
+figure
+plot(P_1)
+ylabel('dBW')
+figure
+histogram(P_1)
+
+figure
+plot(P_2)
+ylabel('dBW')
+figure
+histogram(P_2)
